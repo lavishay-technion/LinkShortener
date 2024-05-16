9bdc4716c0770d96dc6efe3f8e325150e33a27a9
Avishay_Home <lavishay@campus.technion.ac.il>
2024-05-16 13:52:13 +0300

    s

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 from link_shortener import LinkShortener
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Response, status
 from fastapi.responses import RedirectResponse
 
 
 app = FastAPI()
 
 @app.get("/api/health_check")
-def health_check(r: Request):
-    return "healthy"
+def health_check(r: Request, response: Response):
+    # response=requests.Request.get("http://127.0.0.1:8000/api/health_check")
+    print(f'The response code is: {response.status_code}')
+    print('')
+    return(f'{status.HTTP_200_OK}: Healthy')
 
 @app.get("/api/{id}")
 def health_check(id: str):
@@ -19,3 +22,6 @@ def create_url(params: dict):
     url = params["url"]
     id = LinkShortener.shorten(url)
     return {"id": id}
+
+
+
